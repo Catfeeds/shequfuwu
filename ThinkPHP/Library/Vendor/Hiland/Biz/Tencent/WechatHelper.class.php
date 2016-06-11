5de97c5c4c4f4b8d57d726b0c495af56d3506d84
develope@foxmail.com <develope@foxmail.com>
2016-06-11 19:14:51 +0800

    324

--- ThinkPHP/Library/Vendor/Hiland/Biz/Tencent/WechatHelper.class.php
+++ ThinkPHP/Library/Vendor/Hiland/Biz/Tencent/WechatHelper.class.php
@@ -100,6 +100,7 @@ class WechatHelper
         $url="https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=$appID&secret=$appSecret";
 
         $output= NetHelper::Get($url);
+        dump($output);
 //        curl_setopt($ch, CURLOPT_URL, $url);
 //        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 //        curl_setopt($ch, CURLOPT_HEADER, 0);
