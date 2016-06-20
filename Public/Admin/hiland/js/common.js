7dfc9eada75fbcc9b463c1f14ac686f708eeb553
notinmood <develope@foxmail.com>
2016-06-20 10:08:48 +0800

    都是废物热

--- Public/Admin/hiland/js/common.js
+++ Public/Admin/hiland/js/common.js
@@ -5,18 +5,10 @@
 
 $(document).ready(function(){
     var targetObject= $('.deleteConfirm');
-    // targetObject.attr('onclickmsg',targetObject.attr('onclick'));
-    // var onclickContent= targetObject.attr('onclick');
-    // targetObject.attr('onclick','');
-    // var hrefContent= targetObject.attr('href');
-    //alter(onclickContent);
 
     targetObject.click(function (event) {
         if(confirm('您确认要删除选定的信息吗？')){
-            //$this.attr();
-            if(isNull(onclickContent)|| onclickContent==''){
-                onclickContent;
-            }
+            
         }else{
             event.preventDefault();
         }
