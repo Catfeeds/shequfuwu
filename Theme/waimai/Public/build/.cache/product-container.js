3ab9275a10b039c0fc531702defa6dd427030700
notinmood <develope@live.com>
2016-07-01 19:57:15 +0800

    删除template的临时文件

--- Theme/waimai/Public/build/.cache/product-container.js
+++
@@ -1,8 +0,0 @@
-/*TMODJS:{"version":2,"md5":"308937b218d09b8abe5808875d33fcfb"}*/
-template('product-container',function($data,$filename
-/**/) {
-'use strict';var $utils=this,$helpers=$utils.$helpers,include=function(filename,data){data=data||$data;var text=$utils.$include(filename,data,$filename);$out+=text;return $out;},$out='';$out+='<div id="mainList" class="main"> <div id="items" class="items"> ';
-include("./productList-container");
-$out+=' </div> </div> ';
-return new String($out);
-});
