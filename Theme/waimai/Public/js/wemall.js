2dbd06902b637f3e3b3a627f862b7a8f1f2e0eca
notinmood <develope@live.com>
2016-07-04 06:17:05 +0800

    二恶

--- Theme/waimai/Public/js/wemall.js
+++ Theme/waimai/Public/js/wemall.js
@@ -1459,13 +1459,13 @@ function switchMenu(obj, id) {
             var html = template("productItems", dataSending);
             $("#productInfoItems").html(html);
 
-            // echo.init({
-            //     offset: 100,
-            //     throttle: 250,
-            //     unload: false,
-            //     callback: function (element, op) {
-            //     }
-            // });
+            echo.init({
+                offset: 100,
+                throttle: 250,
+                unload: false,
+                callback: function (element, op) {
+                }
+            });
             // backToTop();
 
 
