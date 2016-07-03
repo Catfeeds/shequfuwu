6cf6cf269ba48b8588dde58f41b053d8e8f0daa6
notinmood <develope@live.com>
2016-07-04 05:49:07 +0800

    是法师打发斯蒂芬

--- Theme/waimai/Public/js/wemall.js
+++ Theme/waimai/Public/js/wemall.js
@@ -1218,15 +1218,15 @@ function openProduct(o) {
 
     initProduct();
 
-    var lastSelectedMenuID = get('lastSelectedMenuID');
-    if (lastSelectedMenuID) {
-        $('#menu_li_' + lastSelectedMenuID).click();
-        //$('.shop-menu li').first().click();
-        //alert(lastSelectedMenuID);
-    } else {
-        $('.shop-menu li').first().click();
-        //alert('nnnnnooooooooooooooo');
-    }
+    // var lastSelectedMenuID = get('lastSelectedMenuID');
+    // if (lastSelectedMenuID) {
+    //     $('#menu_li_' + lastSelectedMenuID).click();
+    //     //$('.shop-menu li').first().click();
+    //     //alert(lastSelectedMenuID);
+    // } else {
+    //     $('.shop-menu li').first().click();
+    //     //alert('nnnnnooooooooooooooo');
+    // }
 
 
     var swiper = new Swiper('#swiper-container', {
