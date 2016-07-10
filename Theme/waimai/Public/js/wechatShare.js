e110f24841106af8e8b3e1b1e097de7afbbac084
notinmood <develope@live.com>
2016-07-10 18:27:30 +0800

    特务团委

---
+++ Theme/waimai/Public/js/wechatShare.js
@@ -0,0 +1,60 @@
+var productID = $("#itemsDetail #detail-id").val();
+
+wx.config({
+    appId: '{$signPackage.appId}',
+    timestamp: '{$signPackage.timestamp}',
+    nonceStr: '{$signPackage.nonceStr}',
+    signature: '{$signPackage.signature}',
+    jsApiList: [
+        'checkJsApi',
+        'onMenuShareTimeline',
+        'onMenuShareAppMessage',
+        'onMenuShareQQ',
+        'onMenuShareWeibo'
+    ]
+})
+;
+wx.ready(function () {
+    // 1 判断当前版本是否支持指定 JS 接口，支持批量判断
+    wx.checkJsApi({
+        jsApiList: [
+            'getNetworkType',
+            'previewImage',
+            'onMenuShareTimeline',
+            'onMenuShareAppMessage',
+            'onMenuShareQQ',
+            'onMenuShareWeibo'
+        ],
+    });
+
+    var pageUrl = window.location.href;
+    var hash = window.location.hash;
+
+    var shareData;
+    var shopData = eval('{$config}');
+
+
+    if (productID) {
+        var imgurl= '';
+        shareData = {
+            title: $('#itemsDetail-container .single-name').html(),
+            desc: $('#itemsDetail-container .detail-title').next().html(),
+            //link: pageUrl,
+            imgUrl: '缩略图',
+        };
+    } else {
+        shareData = {
+            title: shopData.name,
+            desc: shopData.remark,
+            //link: pageUrl,
+            imgUrl: 'http://{$hostName}' + data.uploadsUrl + shopData.savepath + shopData.savename,
+        };
+    }
+
+
+    wx.onMenuShareAppMessage(shareData);
+    wx.onMenuShareTimeline(shareData);
+    wx.onMenuShareQQ(shareData);
+    wx.onMenuShareWeibo(shareData);
+});
+
