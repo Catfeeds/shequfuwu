0cfc6be906e11bb3e241e409f9a1f2da2648e4f3
notinmood <develope@live.com>
2016-08-15 06:16:30 +0800

    撒的范德萨

--- Application/Common/Model/BizHelper.class.php
+++ Application/Common/Model/BizHelper.class.php
@@ -468,7 +468,7 @@ class BizHelper
             $content= "本店活动$redPacket[actionname]进展中，有大批量红包派送，点击这里领取！";
             $packetId= $redPacket['id'];
             $url= WebHelper::getWebAppFull().U("App/Index/sendRedpacket","packetId=$packetId&openId=$openId");
-            $result= "<a href=\"$url\">$content</a>";
+            $result= "<a href='$url'>$content</a>";
             return  $result;
         } else {
             return "";
