3a4dbfcd2ca3b9ae654d01243f26581632fd9b5b
notinmood <develope@live.com>
2016-08-15 07:08:30 +0800

    RTRETRTR

--- Application/Admin/Controller/WechatController.class.php
+++ Application/Admin/Controller/WechatController.class.php
@@ -187,21 +187,21 @@ class WechatController extends Controller
                 }
 
 //                //CommonLoger::log("hongbaorizhi",$openId.'----'.$messageContent);
-                $customerMsgStatus= WechatHelper::responseCustomerServiceText($openId, $messageContent);
-                CommonLoger::log("hongbaorizhiStatus",$customerMsgStatus);
-
-
-//                $messageData = '{
-//                    "touser":"' . $openId . '",
-//                    "msgtype":"text",
-//                    "text":
-//                    {
-//                         "content":"' . $messageContent . '"
-//                    }
-//                }';
-//
-//                $customerMsgStatus= self::$weObj->sendCustomMessage($messageData);
+//                $customerMsgStatus= WechatHelper::responseCustomerServiceText($openId, $messageContent);
 //                CommonLoger::log("hongbaorizhiStatus",$customerMsgStatus);
+
+
+                $messageData = '{
+                    "touser":"' . $openId . '",
+                    "msgtype":"text",
+                    "text":
+                    {
+                         "content":"' . $messageContent . '"
+                    }
+                }';
+
+                $customerMsgStatus= self::$weObj->sendCustomMessage($messageData);
+                CommonLoger::log("hongbaorizhiStatus",$customerMsgStatus);
 
                 $newsArray = self::generateWecomeNewsResponse($merchantScanedID);
                 self::$weObj->news($newsArray)->reply();
