0da38d1cfd31a40908cd61b471c53cdaa5b48024
notinmood <develope@live.com>
2016-10-22 18:00:11 +0800

    34534545454

--- Theme/waimai/Public/js/wemall.js
+++ Theme/waimai/Public/js/wemall.js
@@ -892,8 +892,8 @@ function clickGroupBuyDetail(id) {
             var saleinfo = "本次拼团共" + json.piececount + "份，现已经售出" + json.soldcount + "份。";
             $('#itemsDetail #sale-info').html(saleinfo);
 
-            $('#itemsDetail .addItem.btn-shopping').attr("doCartOfGroupBuy", 'doCart(this ,' + json.id + ',\'' + json.name + '\',' + json.totalprice + ',\'+json.prepayprice +\')');
-            $('#itemsDetail .numbers-add').attr("onclick", 'doCartOfGroupBuy(this ,' + json.id + ',\'' + json.name + '\',' + json.totalprice + ',\'+ json.prepayprice +\')');
+            $('#itemsDetail .addItem.btn-shopping').attr("doCartOfGroupBuy", 'doCart(this ,' + json.id + ',\'' + json.name + '\',' + json.totalprice + ','+json.prepayprice +')');
+            $('#itemsDetail .numbers-add').attr("onclick", 'doCartOfGroupBuy(this ,' + json.id + ',\'' + json.name + '\',' + json.totalprice + ','+ json.prepayprice +')');
             $('#itemsDetail .numbers-minus').attr("onclick", 'reduceGroupBuyNum(this ,' + json.id + ', false)');
 
             $('#items-total-price').html(totalPrice);
